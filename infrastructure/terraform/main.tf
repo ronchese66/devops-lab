c30b411311a556263e89948c6d7086e0eac1aad9
ronchese66 <yronchese@gmail.com>
2025-10-31 19:17:10 +0200

    Switch VPC module to git source

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,4 +1,4 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "git::https://github.com/ronchese66/devops-lab.git//infrastructure/terraform/modules/vpc?ref=v1.0.0"
   project_name = var.project_name
 }
