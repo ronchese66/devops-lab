bb28af2af4f3c5e473a417f7fa744541139775f0
ronchese66 <yronchese@gmail.com>
2025-11-18 06:48:46 +0200

    Minor changes

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,12 +4,13 @@ module "vpc" {
 }
 
 module "eks" {
-  source       = "git::https://github.com/ronchese66/devops-lab.git//infrastructure/terraform/modules/eks?ref=eks-v1.0.0"
+  source       = "git::https://github.com/ronchese66/devops-lab.git//infrastructure/terraform/modules/eks?ref=eks-v1.1.0"
   project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
 }
 
 module "kms" {
-  source       = "git::https://github.com/ronchese66/devops-lab.git//infrastructure/terraform/modules/kms?ref=kms-v1.0.0"
+  source                  = "git::https://github.com/ronchese66/devops-lab.git//infrastructure/terraform/modules/kms?ref=kms-v1.1.0"
   project_name            = var.project_name
   enable_key_rotation     = var.enable_key_rotation
   deletion_window_in_days = var.deletion_window_in_days
