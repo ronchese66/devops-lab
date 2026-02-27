5efe51aca377d1c7506bc601418b9c48949d3da5
ronchese66 <yronchese@gmail.com>
2026-02-27 11:13:24 +0200

    Fixed permissions issue for shared directory. Add Jenkins Agent image, incomplete for now.

--- ci-infra/vagrant/provisioners/create-jenkins-user.sh
+++ ci-infra/vagrant/provisioners/create-jenkins-user.sh
@@ -2,13 +2,17 @@
 
 set -e
 
-useradd -m -s /bin/bash -u 2000 jenkins
-mkdir -p /home/jenkins/.ssh
+useradd -m -s /bin/bash jenkins
 
+mkdir -p /home/jenkins/.ssh
 cat /tmp/id_ed25519.pub > /home/jenkins/.ssh/authorized_keys
 chmod 700 /home/jenkins/.ssh
 chmod 600 /home/jenkins/.ssh/authorized_keys
 chown -R jenkins:jenkins /home/jenkins/.ssh
 
+groupadd -g 2000 jenkins-shared
+usermod -aG jenkins-shared jenkins
+
 mkdir -p /opt/jenkins_workspace
-chown -R jenkins:jenkins /opt/jenkins_workspace
+chown -R jenkins:jenkins-shared /opt/jenkins_workspace
+chmod 2770 /opt/jenkins_workspace
